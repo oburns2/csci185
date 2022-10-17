5b4030de9869ac6d7f7e48ab0871547e941f9bbb
Olivia <oburns2@unca.edu>
2022-10-17 18:01:31 -0400

    HW04

---
+++ homework/hw04/landscapes/sketch.js
@@ -0,0 +1,44 @@
+const canvasWidth = window.innerWidth;
+const canvasHeight = window.innerHeight; 
+
+async function setup() {
+    createCanvas(canvasWidth, canvasHeight);
+    
+let i = 0;
+while (i < 1000) {
+    strokeWeight(0);
+    fill('white');
+    const x = Math.random() * canvasWidth;
+    const y = Math.random() * canvasHeight;
+    const width = Math.random() * 2 + 0.5;
+    circle(x, y, width);
+    ++i;
+}
+
+
+}
+
+
+
+// function drawStars() {
+//     strokeWeight(0);
+//     fill('white');
+//     for (let i = 0; i < 1000; i++) {
+//         const x = Math.random() * canvasWidth;
+//         const y = Math.random() * canvasHeight;
+//         const width = Math.random() * 2 + 0.5;
+//         circle(x, y, width);
+//     }
+// }
+
+// function drawBubbles() {
+//     strokeWeight(1);
+//     stroke('white');
+//     noFill()
+//     for (let i = 0; i < 500; i++) {
+//         const x = Math.random() * canvasWidth;
+//         const y = Math.random() * canvasHeight;
+//         const width = Math.random() * 40 + 5;
+//         circle(x, y, width);
+//     }
+// }
