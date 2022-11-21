b62d80d8c9476a4cebe004e2e883e116dbe027a9
Olivia <oburns2@unca.edu>
2022-11-21 16:43:25 -0500

    spotify

--- homework/hw05/your_task/main.js
+++ homework/hw05/your_task/main.js
@@ -2,8 +2,7 @@ const baseURL = 'https://www.apitutor.org/spotify/simple/v1/search';
 
 function search (ev) {
     const term = document.querySelector('#search').value;
-    console.log('search for:', term);
-    // issue three Spotify queries at once...
+    // console.log('search for:', term);
     getTracks(term);
     getAlbums(term);
     getArtist(term);
@@ -16,41 +15,76 @@ async function getTracks (term) {
     const trackEndpoint = baseURL + "?q=" + term + "&type=track";
     console.log(trackEndpoint);
     const data = await fetch(trackEndpoint).then(response => response.json());
-
     if(data.length === 0){
         document.querySelector('#tracks').innerHTML = "No Results Found";
         return;
     }
-
-    console.log(data[0].name);
-   
+    document.querySelector('#tracks').innerHTML = "";
+    for(let i=0; i < 5; i++){
+    const template =`
+    <section class="track-item preview" onclick="playSong('${data[i].id}')">
+    <img src="${data[i].album.image_url}">
+    <i class="fas play-track fa-play" aria-hidden="true"></i>
+    <div class="label">
+        <h2>${data[i].name}</h2>
+        <p>
+            ${data[i].artist.name}
+        </p>
+    </div>
+</section>`
+    document.querySelector('#tracks').insertAdjacentHTML('beforeend', template);
+    }
 }
+
+
+function playSong(id){
+    const template = `
+        <iframe style="
+            border-radius:12px" 
+            src="https://open.spotify.com/embed/track/${id}?utm_source=generator&theme=0" 
+            width="100%" 
+            height="352" 
+            frameBorder="0" 
+            allowfullscreen="" 
+            allow="autoplay; 
+            clipboard-write; 
+            encrypted-media; 
+            fullscreen; 
+            picture-in-picture" 
+            loading="lazy">
+        </iframe>`
+        document.querySelector('#artist').innerHTML = template;
+}
+
+
+
 
 async function getAlbums (term) {
     const albumEndpoint = baseURL + "?q=" + term + "&type=album";
-    console.log(albumEndpoint);
+    // console.log(albumEndpoint);
     const data = await fetch(albumEndpoint).then(response => response.json());
-
     if(data.length === 0){
         document.querySelector('#albums').innerHTML = "No Results Found";
         return;
     }
-
-    console.log(data[0].name);
 }
+
+
+
+
+
+
 
 async function getArtist (term) {
     const artistEndpoint = baseURL + "?q=" + term + "&type=artist";
-    console.log(artistEndpoint);
+    // console.log(artistEndpoint);
     const data = await fetch(artistEndpoint).then(response => response.json());
-
     if(data.length === 0){
         document.querySelector('#artist').innerHTML = "No Results Found";
         return;
     }
-
-    console.log(data[0].name);
-    console.log(data[0].image_url);
+    // console.log(data[0].name);
+    // console.log(data[0].image_url);
     const template = `
     <section class="artist-card" id="3Nrfpe0tUJi4K4DXYWgMUX">
                             <div>
@@ -69,9 +103,14 @@ async function getArtist (term) {
 };
 
 
+
+
+
+
+
 document.querySelector('#search').onkeyup = function (ev) {
     // Number 13 is the "Enter" key on the keyboard
-    console.log(ev.keyCode);
+    // console.log(ev.keyCode);
     if (ev.keyCode === 13) {
         ev.preventDefault();
         search();
