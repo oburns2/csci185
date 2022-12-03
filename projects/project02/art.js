ee931d6e5e88a3ebaa0d54c3f8d1673b52344959
Olivia <oburns2@unca.edu>
2022-12-03 16:15:30 -0500

    Final

--- projects/project02/art.js
+++ projects/project02/art.js
@@ -1,26 +1,26 @@
 const art = [
-    {image_url:"art/revaandholly.jpg"},
-    {image_url:"art/ancient.png"},
-    {image_url:"art/aspen.jpg"},
-    {image_url:"art/rhys.jpg"},
-    {image_url:"art/cori.jpg"},
-    {image_url:"art/nirvana.jpg"},
-    {image_url:"art/styx.jpg"},
-    {image_url:"art/adrian.png"},
-    {image_url:"art/rain.jpg"},
-    {image_url:"art/soul.jpg"},
-    {image_url:"art/aben.jpg"},
-    {image_url:"art/fall.png"},
-    {image_url:"art/desatur.jpg"},
-    {image_url:"art/light.jpg"},
-    {image_url:"art/shadow.jpg"},
+    {image_url:"art/revaandholly.jpg", alt:"Reva and Holly in the Prairie"},
+    {image_url:"art/ancient.png", alt:"Four of my characters"},
+    {image_url:"art/aspen.jpg", alt:"Aspen with a rose"},
+    {image_url:"art/rhys.jpg", alt:"Rhys playing guitar"},
+    {image_url:"art/cori.jpg", alt:"Cori in the snow"},
+    {image_url:"art/nirvana.jpg", alt:"Nirvana in the water"},
+    {image_url:"art/styx.jpg", alt:"Styx braiding flowers into his hair"},
+    {image_url:"art/adrian.png", alt:"Adrian wearing glasses"},
+    {image_url:"art/rain.jpg", alt:"Reva in the rain"},
+    {image_url:"art/soul.jpg", alt:"Holly holding a star"},
+    {image_url:"art/aben.jpg", alt:"Doodles of Aspen"},
+    {image_url:"art/fall.png", alt:"Rhys falling"},
+    {image_url:"art/desatur.jpg", alt:"Cori waving"},
+    {image_url:"art/light.jpg", alt:"Nirvana with a light coming from her eye"},
+    {image_url:"art/shadow.jpg", alt:"Styx with Nirvana in the background"},
   ];
 
 
   for(let i = 0; i < art.length; i++){
     const template=`
-    <img class="drawing" src="${art[i].image_url}">`
-
-    console.log("yo mama");
+    <img class="drawing" alt="${art[i].alt}" src="${art[i].image_url}">`
     document.querySelector('.art-container').insertAdjacentHTML ('beforeend', template);
   }
+
+  $(document).ready(function(){$("img").click(function(){this.requestFullscreen()})});
